00eb7866ec988e3b4ce07a0dafa390c5398267b8
user <user@Users-MacBook.local>
2020-09-21 22:03:24 +0100

    add database connection

--- utilities/pg-config.ts
+++ utilities/pg-config.ts
@@ -1,14 +1,10 @@
-import connect, { sql } from '@databases/pg'
+import postgres from "postgres"
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-const db = connect();
+const connectionstring = process.env.DATABASE_URL as string; 
 
+const sql = postgres(connectionstring);
 
-async function auth() {
-    await db.query(sql`SELECT 1=1`)
-    console.log('Server started succesfully'); 
-}
-
-export default { auth, db, sql };
+export default sql
