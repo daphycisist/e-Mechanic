1843c316366f09c0e5b0799aa1bd9d3bf2c2c11d
user <user@Users-MacBook.local>
2020-09-22 14:40:48 +0100

    login and signup validation

--- validations/validations.ts
+++ validations/validations.ts
@@ -2,14 +2,15 @@ import { loginSchema, signupSchema } from "../schema/joiSchemas";
 
 const validateLoginInput = (userInput: object) => {
   const { error, value } = loginSchema.validate(userInput);
-  if (error) throw new Error("invalid input" + error);
+  if (error) throw new Error(error.details[0].message);
   return value;
 };
 
 const validateSignupInput = (userInput: object) => {
   const { error, value } = signupSchema.validate(userInput);
-  if (error) throw new Error("invalid input xxxx  " + error);
+  if (error) throw new Error(error.details[0].message)
   return value;
 };
 
 export { validateLoginInput, validateSignupInput };
+
