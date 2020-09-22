bdcba684063660e1f280a02c3eb2dc1f3bb5b987
user <user@Users-MacBook.local>
2020-09-22 14:41:38 +0100

    update users joi schema

--- schema/joiSchemas.ts
+++ schema/joiSchemas.ts
@@ -1,13 +1,13 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 const signupSchema = Joi.object().keys({
-  // firstname: Joi.string().lowercase().trim().required,
-  // lastname: Joi.string().lowercase().trim().required,
+  firstname: Joi.string().lowercase().trim().required(),
+  lastname: Joi.string().lowercase().trim().required(),
   email: Joi.string().email().min(8).max(100).lowercase().trim().required(),
   phoneNumber: Joi.number().required(),
   password: Joi.string().min(7).alphanum().required(),
-  // confirmPassword: Joi.valid(Joi.ref('password')).required
-}); 
+  confirmPassword: Joi.valid(Joi.ref("password")).required(),
+});
 
 const loginSchema = Joi.object().keys({
   email: Joi.string().email().lowercase().required(),
