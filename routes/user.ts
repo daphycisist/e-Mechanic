9a03e03774292f060865a9c22dccf11156812625
user <user@Users-MacBook.local>
2020-10-01 23:58:36 +0100

    add user signup and login

--- routes/user.ts
+++ routes/user.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
+import Controller from "../controller/controller";
 
 const router = express.Router();
 
 router.get("/user", function userPage(_req: Request, res: Response) {
   res.send("Hope is alive man");
 });
+
+router.post("/signup", Controller.signup)
+router.post("/login", Controller.login);
+
 
 export default router;
